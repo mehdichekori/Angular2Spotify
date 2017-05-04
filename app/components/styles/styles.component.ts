d9822bb54e8407d2a7dc8bcc9913b3b5e477340b
Mehdi chekori <mehdichekori@gmail.com>
2017-05-04 14:22:18 -0400

    Add files via upload

--- app/components/styles/styles.component.ts
+++ app/components/styles/styles.component.ts
@@ -1,5 +1,6 @@
 import { parentComponent } from './../Child-Parent/parent.component';
 import { Component, OnInit } from '@angular/core';
+import {Observable} from 'rxjs/Rx';
 import * as _ from "lodash";
 
 @Component({
@@ -14,12 +15,26 @@ export class StylesComponent implements OnInit {
     size = 16;
     colors = Array<string>();
     backup = Array<string>();
+    ticks = 0;
+    c = 0;
 
     ngOnInit() {
         this.colors.push('Green','Purple','Blue','Black','Orange');
         this.backup = this.colors;
-    }
 
+        // let timer = Observable.timer(0,1000);
+        // timer.subscribe(t=>this.ticks = t);
+        setInterval(() => {
+            this.ticks = (new Date().getTime()/1000- new Date("2017-05-04:12:45:00Z").getTime()/1000)
+            
+            this.hh = Math.floor(this.ticks / 3600);
+            this.leftover = this.ticks % 3600;
+            this.mm = Math.floor(this.leftover/ 60);
+            this.ss = (this.ticks % 60).toFixed(0);
+            this.c = this.ticks * 0.005555555555555555;
+        //console.log(this.hh+'h'+this.mm+'m'+this.ss+'s',this.c.toFixed(2)+'$');     
+     }, 1000);
+    }
     changeColor(col:string){
         this.color = col;
     }
@@ -31,6 +46,5 @@ export class StylesComponent implements OnInit {
     resetArray(){
         this.colors = this.backup;
     }
-
  }
 
